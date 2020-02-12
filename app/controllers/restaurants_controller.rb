33cd04dfe608d9b51e20ce8c945a5188b020da62
Thomas Rose <thomas.c.n.rose@gmail.com>
2020-02-12 11:41:09 +0100

    controller created

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @task = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to Restaurants_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to restaurants_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_path
+  end
+
+  def done
+    completed == true
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:title, :details, :completed)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "012456775", category: "British" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "012456775", category: "Italien" }
+Chez_mario =  { name: "Chez Mario", address: "5 rue de Bizerte, 75017 Paris", phone_number: "012456775", category: "Italien" }
+Nefis =  { name: "Le petit Gourmant", address: "35 rue Championnet, 75018 Paris", phone_number: "012456775", category: "French" }
+DimSUm =  { name: "Dim Sum", address: "45 rue des Dames, 75017 Paris", phone_number: "012456775", category: "Asian" }
+
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
